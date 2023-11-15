61e2b7dd46ab266de6811c8ffe4a409a1cd83bf8
niwatoro <girlwithskyhairpin@gmail.com>
2023-11-16 00:39:12 +0900

    Remove unnecessary files and update code style

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import { Alchemy, Network } from 'alchemy-sdk';
-import { useEffect, useState } from 'react';
+import { Alchemy, Network } from "alchemy-sdk";
+import { useEffect, useState } from "react";
 
-import './App.css';
+import "./App.css";
 
 // Refer to the README doc for more information about using API
 // keys in client-side code. You should never do this in production
@@ -10,7 +10,6 @@ const settings = {
   apiKey: process.env.REACT_APP_ALCHEMY_API_KEY,
   network: Network.ETH_MAINNET,
 };
-
 
 // In this week's lessons we used ethers.js. Here we are using the
 // Alchemy SDK is an umbrella library with several different packages.
@@ -21,6 +20,7 @@ const alchemy = new Alchemy(settings);
 
 function App() {
   const [blockNumber, setBlockNumber] = useState();
+  const [transactions, setTransactions] = useState([]);
 
   useEffect(() => {
     async function getBlockNumber() {
@@ -30,7 +30,43 @@ function App() {
     getBlockNumber();
   });
 
-  return <div className="App">Block Number: {blockNumber}</div>;
+  useEffect(() => {
+    async function getTransactions() {
+      const transactions = (
+        await alchemy.core.getBlockWithTransactions(blockNumber)
+      ).transactions;
+      setTransactions(transactions);
+    }
+
+    getTransactions();
+  }, [blockNumber]);
+
+  return (
+    <div className="App">
+      <div>Block Number: {blockNumber}</div>
+      <div>
+        Transactions:
+        <table>
+          <thead>
+            <tr>
+              <th>Hash</th>
+              <th>From</th>
+              <th>To</th>
+            </tr>
+          </thead>
+          <tbody>
+            {transactions.map((transaction) => (
+              <tr key={transaction.hash}>
+                <td>{transaction.hash}</td>
+                <td>{transaction.from}</td>
+                <td>{transaction.to}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
 
 export default App;
